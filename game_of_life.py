f82459f4ab0963b428efbfb6c46dd686b84cf121
Lukasz Szymanski <Luk_szym@wp.pl>
2020-10-19 07:57:36 +0200

    initial commit

---
+++ game_of_life.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+
+
+def step(board):
+    """ return the next generation board """
+    pass
+
+
+def create_board(x, y):
+    """ create board with x-columns and y-rows """
+    pass
+
+
+class GameOfLife(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        # create main window
+
+    def board(self):
+        """ Create the board window """
+        pass
+
+    def info(self):
+        """ show the game rules """
+
+    def exit(self):
+        """ exit the game """
+
+    def initial(self):
+        """ manage the initial pattern """
+
+        
